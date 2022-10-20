f651bc8dd7b86509751cabaf2b0eeeddcbc97265
willy serge <willyserge01@gmail.com>
2022-10-20 09:29:32 +0200

    integrate craft demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
-import "./style.css";
+// pages/index.js
+
+import React from 'react';
+import {Typography, Paper, Grid} from '@material-ui/core';
+
+import { Toolbox } from './Components/Toolbox';
+import { SettingsPanel } from './Components/SettingsPanel';
+import { Topbar } from './Components/Topbar';
+
+import { Container } from './Components/User/Container';
+import { Button } from './Components/User/Button';
+import { Card } from './Components/User/Card';
+import { Text } from './Components/User/Text';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={{margin: "0 auto", width: "800px"}}>
+      <Typography variant="h5" align="center">A super simple page editor</Typography>
+      <Grid container spacing={3} style={{paddingTop: "10px"}}>
+        <Topbar />
+        <Grid item xs>
+          <Container padding={5} background="#eee">
+            <Card />
+          </Container>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>
+              <Toolbox />
+              <SettingsPanel />
+          </Paper>          
+        </Grid>
+      </Grid>
     </div>
   );
 }
